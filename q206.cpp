321bc13db550986258885da3a4bda290ef28f204
JieHong-Liu <jiehong0914@gmail.com>
2021-04-29 15:04:18 +0800

    add Reverse linked list (q.206)

---
+++ q206.cpp
@@ -0,0 +1,28 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution
+{
+public:
+    ListNode *reverseList(ListNode *head)
+    {
+        ListNode *lastPtr = NULL;
+        ListNode *nowPtr = head;
+        while (nowPtr) // nowPtr != NULL
+        {
+            ListNode *last_lastPtr = lastPtr;
+            lastPtr = nowPtr;
+            nowPtr = nowPtr->next;
+            lastPtr->next = last_lastPtr;
+        }
+        head = lastPtr;
+        return head;
+    }
+};
